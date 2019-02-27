2a7a8e97a0f65556e036a5b90326b53964a73257
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-02-27 16:38:40 -0500

    Fix: Created default scope on products model, updated products route.

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -4,33 +4,41 @@ const db = require('../db')
 
 // PRODUCTS MODEL
 
-const Product = db.define('product', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
+const Product = db.define(
+  'product',
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    price: {
+      type: Sequelize.INTEGER, // Stored in cents.
+      allowNull: false
+    },
+    imageUrl: {
+      type: Sequelize.STRING,
+      defaultValue: '/images/blackHoodie.png'
+    },
+    description: {
+      type: Sequelize.TEXT,
+      defaultValue: 'Buy this product!'
+    },
+    size: {
+      type: Sequelize.ENUM('S', 'M', 'L', 'XL', 'XXL'),
+      defaultValue: 'M'
+    },
+    merchant: {
+      type: Sequelize.STRING,
+      default: 'Misfit'
+    },
+    quantity: Sequelize.INTEGER
   },
-  price: {
-    type: Sequelize.INTEGER, // Stored in cents.
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    defaultValue: '/images/blackHoodie.png'
-  },
-  description: {
-    type: Sequelize.TEXT,
-    defaultValue: 'Buy this product!'
-  },
-  size: {
-    type: Sequelize.ENUM('S', 'M', 'L', 'XL', 'XXL'),
-    defaultValue: 'M'
-  },
-  merchant: {
-    type: Sequelize.STRING,
-    default: 'Misfit'
-  },
-  quantity: Sequelize.INTEGER
-})
+  {
+    defaultScope: {
+      attributes: ['id', 'name', 'price', 'description', 'imageUrl', 'size']
+    }
+  }
+)
 
 // EXPORT
 
